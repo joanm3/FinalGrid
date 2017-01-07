993f8d46857bdbad0e6e4ee366f183a8371a1f44
Joan Mitjans <Joan Mitjans>
2017-01-07 16:39:58 +0100

    Copied From other project.

---
+++ Assets/Editor/LevelGridWindow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor; 
+using System.Collections;
+
+public class LevelGridWindow : EditorWindow
+{
+    LevelGrid m_levelGrid = null; 
+
+    public void Init()
+    {
+        m_levelGrid = LevelGrid.Ins; 
+    }
+
+    void OnGUI()
+    {
+        if (m_levelGrid == null)
+        {
+            Init(); 
+        }
+        //m_levelGrid.hideUnityHandles = EditorGUILayout.Toggle("Hide Unity Handles:", m_levelGrid.hideUnityHandles); 
+        EditorGUILayout.PrefixLabel("Grid Pow:"); 
+        m_levelGrid.gridSize = (LevelGrid.Pow2)EditorGUILayout.EnumPopup(m_levelGrid.gridSize);
+        EditorGUILayout.PrefixLabel("Height Level:");
+        m_levelGrid.height = EditorGUILayout.FloatField(m_levelGrid.heightIndex);
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Controls:"); 
+        EditorGUILayout.HelpBox("Ctrl + move: copy", MessageType.None);
+        EditorGUILayout.HelpBox("Y: move height up", MessageType.None);
+        EditorGUILayout.HelpBox("U: move height down", MessageType.None);
+        EditorGUILayout.HelpBox("A: rotate object 90º", MessageType.None);
+        EditorGUILayout.HelpBox("Use Rectbox for better Usage. Rectbox shortcut: T", MessageType.Info); 
+
+
+        //m_levelGrid.Update(); 
+    }
+
+}
---
+++ Assets/Editor/SnapToGridEditor.cs
@@ -0,0 +1,209 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(SnapToGrid))]
+public class SnapToGridEditor : Editor
+{
+    SnapToGrid m_myTarget;
+    bool m_instantiated = false;
+    private bool m_controlPressed = false;
+    private bool m_aPressed = false;
+    private bool m_shiftPressed = false;
+    private bool m_leftMousePressed = false;
+    float distance;
+    Vector3 gridPos = new Vector3();
+    GameObject onMouseOverGameObject;
+    bool isThisObject = false;
+    bool isMouseDown = false;
+
+    //GameObject m_instantiatedGameObject = new GameObject(); 
+
+    private void OnEnable()
+    {
+        m_instantiated = false;
+        SceneView.onSceneGUIDelegate += EventHandler;
+    }
+
+    private void OnDisable()
+    {
+        m_instantiated = false;
+        SceneView.onSceneGUIDelegate -= EventHandler;
+    }
+
+    private void EventHandler(SceneView sceneview)
+    {
+        if (m_myTarget == null)
+            m_myTarget = target as SnapToGrid;
+
+
+        if (m_myTarget.childToApplyBoxCollider != null)
+        {
+            BoxCollider _boxCollider = m_myTarget.GetComponent<BoxCollider>();
+            _boxCollider.size = m_myTarget.childToApplyBoxCollider.bounds.size;
+            _boxCollider.center = m_myTarget.childToApplyBoxCollider.bounds.center;
+
+        }
+
+
+        HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+        Ray worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+
+        RaycastHit[] hits = Physics.RaycastAll(worldRay, 1000);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+
+            if (hits[i].transform.gameObject.layer == LayerMask.NameToLayer("Grid"))
+            {
+                gridPos = hits[i].point;
+            }
+            else if (hits[i].transform.GetComponent<SnapToGrid>() != null)
+            {
+                //Debug.Log("Object name: " + hits[i].collider.name);
+                onMouseOverGameObject = hits[i].transform.gameObject;
+            }
+        }
+
+        if (hits.Length < 1)
+            onMouseOverGameObject = null;
+
+        //Debug.Log(onMouseOverGameObject);
+
+        UpdateKeyEvents();
+
+        //mouse position in the grid
+        float col = (float)gridPos.x / ((float)LevelGrid.Ins.gridSize * LevelGrid.Ins.scaleFactor);
+        float row = (float)gridPos.z / ((float)LevelGrid.Ins.gridSize * LevelGrid.Ins.scaleFactor);
+
+        LevelGrid.Ins.UpdateInputGridHeight();
+
+
+        if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+        {
+
+            isMouseDown = true;
+            if (onMouseOverGameObject == null)
+                return;
+
+            if (onMouseOverGameObject == m_myTarget.gameObject)
+            {
+                isThisObject = true;
+            }
+            else
+            {
+                isThisObject = false;
+                if (onMouseOverGameObject != null)
+                    Selection.activeGameObject = onMouseOverGameObject;
+                else
+                    Selection.activeGameObject = null;
+            }
+
+
+        }
+
+
+        if (isMouseDown && m_aPressed)
+        {
+            LevelGrid.Ins.selectedGameObject.transform.eulerAngles += new Vector3(0, 90f, 0);
+            m_aPressed = false;
+        }
+
+        //mouse click and dragandrop
+        if (Event.current.type == EventType.MouseDrag && Event.current.button == 0)
+        {
+            if (Selection.activeGameObject == null)
+                return;
+            if (m_aPressed)
+            {
+                LevelGrid.Ins.selectedGameObject.transform.eulerAngles += new Vector3(0, 90f, 0);
+                m_aPressed = false;
+            }
+            SnapToGrid((int)col, (int)row, LevelGrid.Ins.height);
+        }
+        ////Debug.Log(m_shiftPressed);
+        //Debug.Log("Event.current.keyCode: " + Event.current.keyCode);
+
+        //if mouse released when control pressed, make a copy / otherwise, destroy old object. 
+        if (Event.current.type == EventType.MouseUp && Event.current.button == 0)
+        {
+            m_instantiated = false;
+            isMouseDown = false;
+            if (LevelGrid.Ins.selectedGameObject)
+            {
+                //make copy if control is pressed
+                if (!m_controlPressed)
+                {
+                    Undo.IncrementCurrentGroup();
+                    Undo.DestroyObjectImmediate(m_myTarget.gameObject);
+                }
+                Selection.activeGameObject = LevelGrid.Ins.selectedGameObject;
+            }
+        }
+    }
+
+    private void UpdateKeyEvents()
+    {
+        if ((Event.current.type == EventType.keyDown) && (Event.current.keyCode == KeyCode.A || Event.current.keyCode == KeyCode.S))
+            m_aPressed = true;
+
+        if ((Event.current.type == EventType.keyUp) && (Event.current.keyCode == KeyCode.A || Event.current.keyCode == KeyCode.S))
+            m_aPressed = false;
+
+        //check if control is pressed. 
+        if ((Event.current.type == EventType.keyDown) && (Event.current.keyCode == KeyCode.LeftControl || Event.current.keyCode == KeyCode.RightControl))
+            m_controlPressed = true;
+
+        if ((Event.current.type == EventType.keyUp) && (Event.current.keyCode == KeyCode.LeftControl || Event.current.keyCode == KeyCode.RightControl))
+            m_controlPressed = false;
+
+        if ((Event.current.type == EventType.keyDown) && (Event.current.keyCode == KeyCode.LeftCommand || Event.current.keyCode == KeyCode.LeftApple))
+            m_shiftPressed = true;
+
+        //if ((Event.current.type == EventType.keyUp) && (Event.current.keyCode == KeyCode.LeftShift || Event.current.keyCode == KeyCode.RightShift))
+        //    m_shiftPressed = false;
+
+
+    }
+
+    private void SnapToGrid(int col, int row, float height)
+    {
+        if (m_myTarget == null)
+            m_myTarget = target as SnapToGrid;
+
+        if (!LevelGrid.Ins.snapToGrid)
+            return;
+
+        // Check out of bounds and if we have a piece selected
+        //if (!LevelGrid.Ins.IsInsideGridBounds(col, row))
+        //    return;
+
+        GameObject obj = m_myTarget.gameObject;
+        if (!m_instantiated)
+        {
+
+            if (PrefabUtility.GetPrefabParent(Selection.activeObject) != null)
+            {
+                obj = PrefabUtility.InstantiatePrefab(PrefabUtility.GetPrefabParent(Selection.activeObject) as GameObject) as GameObject;
+                obj.transform.rotation = m_myTarget.gameObject.transform.rotation;
+
+            }
+            else
+            {
+                //Debug.Log("prefab parent not found");
+                obj = Instantiate(m_myTarget.gameObject);
+            }
+
+            obj.name = m_myTarget.gameObject.name;
+            if (LevelGrid.Ins.parentGameObject != null) obj.transform.parent = LevelGrid.Ins.parentGameObject;
+
+            LevelGrid.Ins.selectedGameObject = obj;
+
+            m_instantiated = true;
+            Undo.RegisterCreatedObjectUndo(obj, "Create " + obj.name);
+        }
+        LevelGrid.Ins.selectedGameObject.transform.position = LevelGrid.Ins.GridToWorldCoordinates(col, row, height);
+        Undo.IncrementCurrentGroup();
+    }
+}
